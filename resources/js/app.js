9f6550d3ff55266effbf5b01dd029491a05e5c26
anto991nio <antonio.91.cardone@gmail.com>
2021-07-02 09:06:55 +0200

    modifica app layaout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,3 @@
 require('./bootstrap');
 
-window.addEventListener("load", function () {
-  const deleteForms = document.querySelectorAll(".delete_form");
 
-  deleteForms.forEach(form => {
-
-    form.addEventListener("submit", (event) => {
-
-      if (!confirm("Sei sicuro di voler cancellare l'utente selezionato?")) {
-        event.preventDefault();
-      }
-
-    });
-
-  });
-
-});
