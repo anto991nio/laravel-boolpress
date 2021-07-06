6b74fef90012373b96ae33dd8d558e7cb879002f
anto991nio <antonio.91.cardone@gmail.com>
2021-07-06 16:37:53 +0200

    Corregge function filter

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -8,6 +8,28 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+
+    //filtro
+    function filter($array, $filter) {
+        $array_filtered = [];
+        if($filter == "") {
+            return $array;
+        }
+        foreach($array as $item) {
+
+            $result = strpos(strtolower($item['category']), $filter);
+
+
+            if($result !== false) {
+                $array_filtered[] = $item;
+            };
+
+        }
+
+        return $array_filtered;
+
+    }
     public function index(){
         // $posts = Post::all();
         $posts = Post::with('category')->with('tags')->get();
@@ -16,30 +38,11 @@ class PostController extends Controller
 
 
 
-        //filtro
-        function filter($posts, $filter) {
-            $array_filtered = [];
-            if($filter == "") {
-                return $posts;
-            }
-            foreach($posts as $post) {
-
-                $result = strpos(strtolower($post['category']), $filter);
-
-
-                if($result !== false) {
-                    $array_filtered[] = $post;
-                };
-
-            }
-
-            return $array_filtered;
-
-        };
+        
 
 
         return response()->json([
-            'result' => filter($posts, $category_filter),
+            'result' => $this->filter($posts, $category_filter),
         ]);
     }
 }
